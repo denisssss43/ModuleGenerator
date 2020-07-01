966071234311a176e06c524a3d990ebfb838efd1
Денис Карасев <karasev.bpi1701@gmail.com>
2020-07-02 01:03:42 +0700

    ...

--- ModuleGenerator.py
+++ ModuleGenerator.py
@@ -1,13 +1,52 @@
 import json
 import codecs
 import os
+import pyodbc
+import re 
+
+def CreateModuleInDB(name):
+
+	with codecs.open('connection.json', 'r', 'utf-8') as read_file:
+		data = json.load(read_file)
+
+		connectionstr = 'DRIVER={0};SERVER={1};DATABASE={2};UID={3};PWD={4}'.format(
+			"{SQL Server\}",
+			str(data['SERVER']), # SERVER, 
+			str(data['DATABASE']), # DATABASE, 
+			str(data['User']), # User, 
+			str(data['Password'])) # Password
+		
+		print(connectionstr)
+
+	cnxn = pyodbc.connect(connectionstr) # Подключение
+	cursor = cnxn.cursor()
+
+	sqlGetGuid = "SELECT TOP (1) [Guid] FROM [GZ].[dbo].[SystemModules] WHERE [Title] LIKE N'{0}' AND IsDeleted = 0;".format(name)
+	sqlAddModule = "INSERT INTO [dbo].[SystemModules]([Guid],[Type],[Title],[DateCreated],[IsDeleted],[DateDeleted]) VALUES (NEWID(), N'4A3BEF77-574D-472F-9502-060E20F01B43', N'{0}', SYSDATETIMEOFFSET(), 0, NULL);".format(name)
+
+	cursor.execute(sqlGetGuid)
+	result = cursor.fetchone()
+
+	if result == None: 
+		cursor.execute(sqlAddModule)
+		cnxn.commit()  
+
+		cursor.execute(sqlGetGuid)
+		result = cursor.fetchone()
+
+	guid = result[0]
+
+
+	return guid
 
 
 with codecs.open('cfg.json', 'r', 'utf-8') as read_file:
 	data = json.load(read_file)
-	Guid = str(data['Guid']) # @Guid_
+	# Guid = str(data['Guid']) # @Guid_
 	Name = str(data['Name']) # @Name_
 	Title = str(data['Title']) # @Title_
+
+Guid = CreateModuleInDB(Title)
 
 # Создание директории модуля
 try: os.mkdir('./{0}'.format(Name))

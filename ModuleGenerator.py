8be23e18badfdf1a97980a2443f85fd69b01135f
Денис Карасев <karasev.bpi1701@gmail.com>
2020-07-02 09:35:44 +0700

    ...

--- ModuleGenerator.py
+++ ModuleGenerator.py
@@ -55,14 +55,14 @@ except: pass
 # Загрузка шаблона вьюмодели
 ViewModel = codecs.open('_temp/Temp_ViewModel.cs', 'r', 'utf-8').read().replace('@Guid_', Guid).replace('@Name_', Name).replace('@Title_', Title)
 # Сохранение шаблона вьюмодели
-codecs.open('{0}/{0}ViewModel.cs'.format(Name), 'w', 'utf-8').write(ViewModel)
+codecs.open('Modules/{0}/{0}ViewModel.cs'.format(Name), 'w', 'utf-8').write(ViewModel)
 
 # Загрузка шаблона XAML
 ViewXaml = codecs.open('_temp/Temp_Window.xaml', 'r', 'utf-8').read().replace('@Guid_', Guid).replace('@Name_', Name).replace('@Title_', Title)
 # Сохранение шаблона XAML
-codecs.open('{0}/{0}Window.xaml'.format(Name), 'w', 'utf-8').write(ViewXaml)
+codecs.open('Modules/{0}/{0}Window.xaml'.format(Name), 'w', 'utf-8').write(ViewXaml)
 
 # Загрузка шаблона XAML.CS
 View = codecs.open('_temp/Temp_Window.xaml.cs', 'r', 'utf-8').read().replace('@Guid_', Guid).replace('@Name_', Name).replace('@Title_', Title)
 # Сохранение шаблона XAML.CS
-codecs.open('{0}/{0}Window.xaml.cs'.format(Name), 'w', 'utf-8').write(View)
+codecs.open('Modules/{0}/{0}Window.xaml.cs'.format(Name), 'w', 'utf-8').write(View)
